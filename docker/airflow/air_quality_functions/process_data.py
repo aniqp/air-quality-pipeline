359041c07d13bb3bf1362bc622785e6c70d490f8
aniqp <aniqpremji@gmail.com>
2023-07-23 13:16:32 -0400

    added handling of latest dates, times and values for pm25

--- docker/airflow/air_quality_functions/process_data.py
+++ docker/airflow/air_quality_functions/process_data.py
@@ -10,4 +10,14 @@ def map_column_to_hour(col):
 
     return new_time
 
+def map_hour_to_column(hour):
+    hour_mapping = {
+        f'H{i:02d}': f'{i:02d}:00'
+        for i in range(24)
+    }
+    format_time = hour_mapping[col]
+    new_time = datetime.strptime(format_time, '%H:%M').time()
+
+    return new_time
+
 print(map_column_to_hour('H10'))
--- docker/airflow/dags/mysql_dag.py
+++ docker/airflow/dags/mysql_dag.py
@@ -2,7 +2,7 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from dateutil.parser import parse
 from datetime import timezone
 from airflow import DAG
@@ -15,7 +15,7 @@ from air_quality_functions.retrieve_data import get_data
 # Default arguments for the DAG
 default_args = {
     'owner': 'aniqpremji',
-    'start_date': datetime(2023, 7, 21),
+    'start_date': datetime(2023, 7, 22),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
@@ -28,6 +28,9 @@ dag = DAG(
 )
 
 def query_latest_pm25(ti):
+    """
+        Method to retrieve the latest date, and the name of the column with the latest time from the database
+    """
     hook = MySqlHook(mysql_conn_id='mysql_conn')
     query_pm25 = """SELECT
                         date_column,
@@ -63,17 +66,52 @@ def query_latest_pm25(ti):
 
     results = hook.get_records(query_pm25)
 
-    for date, time in results:
-        format_time = map_column_to_hour(time)
-        new_datetime = datetime.combine(date, format_time)
+    (pm25_date, pm25_time) = results[0]
+    format_time = map_column_to_hour(pm25_time)
+    new_datetime = datetime.combine(pm25_date, format_time)
     
     print(f'Datetime: {new_datetime}')
     dt_str = new_datetime.isoformat()
+    pm25_date_str = pm25_date.isoformat()
+
+    ti.xcom_push(key = 'latest_pm25_date_col', value = pm25_date_str)
+    ti.xcom_push(key = 'latest_pm25_time_col', value = pm25_time)
     ti.xcom_push(key = 'latest_pm25_datetime', value = dt_str)
 
-def get_latest_pm25_result(ti):
-    result = get_data(url = "https://api.openaq.org/v2/latest?parameter=pm25&parameter=o3&location=Kitchener",
+def get_latest_pm25_api(ti):
+    """
+        Get latest information from the OpenAQ api
+    """
+    latest_api = get_data(url = "https://api.openaq.org/v2/latest?parameter=pm25&parameter=o3&location=Kitchener",
             headers = {"accept": "application/json", "X-API-Key": "bf94e16e413120ef454855fc046f5018c262c450b0ee9e976e31b4f5fad116e9"})
+    ti.xcom_push(key = 'latest_api', value = latest_api)    
+
+def get_value_for_latest_pm25(ti):
+    """
+        For previously defined latest date and time, get pm25 value
+    """
+    hook = MySqlHook(mysql_conn_id='mysql_conn')
+    latest_pm25_date = ti.xcom_pull(task_ids = 'check_latest_pm25', key = 'latest_pm25_date_col')
+    latest_pm25_date = datetime.strptime(latest_pm25_date, "%Y-%m-%d").date()
+    latest_pm25_time_col = ti.xcom_pull(task_ids = 'check_latest_pm25', key = 'latest_pm25_time_col')
+
+
+    # Protect against SQL injections
+    valid_columns = [f'H{i:02d}' for i in range(24)]
+    if latest_pm25_time_col not in valid_columns:
+        raise ValueError(f"Invalid column name: {latest_pm25_time_col}")
+
+    query_latest_value = f"""
+        SELECT {latest_pm25_time_col} FROM kitchener_pm25
+        WHERE date_column = %s
+    """
+
+    result = hook.get_first(query_latest_value, parameters=(latest_pm25_date,))[0]
+
+    ti.xcom_push(key = 'latest_pm25_value', value = result)
+
+def find_time_difference(ti):
+    result = ti.xcom_pull(task_ids = 'get_latest_result', key ='latest_api')
     latest_datetime = ti.xcom_pull(task_ids = 'check_latest_pm25', key = 'latest_pm25_datetime')
     latest_datetime = datetime.strptime(latest_datetime, "%Y-%m-%dT%H:%M:%S")
 
@@ -81,20 +119,47 @@ def get_latest_pm25_result(ti):
     formatted_datetime_str = utc_datetime.strftime("%Y-%m-%d %H:%M")
     new_datetime = datetime.strptime(formatted_datetime_str, "%Y-%m-%d %H:%M")
 
-    time_diff = (new_datetime - latest_datetime).total_seconds()/3600
+    time_diff = str((new_datetime - latest_datetime).total_seconds()/3600)
     print(f'Time diff: {time_diff} hours')
-    
 
-get_pm25_query_results_task = PythonOperator(
+    ti.xcom_push(key = 'time_diff', value = time_diff)
+    
+def handle_data_upload(ti, **kwargs):
+    latest_api_value = ti.xcom_pull(task_ids = 'check_latest_pm25_value', key = 'latest_pm25_value')
+    latest_pm25_date = date(ti.xcom_pull(task_ids = 'check_latest_pm25', key = 'latest_pm25_date_col'))
+    latest_pm25_time_col = ti.xcom_pull(task_ids = 'check_latest_pm25', key = 'latest_pm25_time_col')    
+    time_diff = int(ti.xcom_pull(task_ids = 'find_pm25_time_difference', key = 'time_diff'))
+
+    if time_diff <= 1:
+        query = f"""
+                UPDATE kitchener_pm25
+                SET {latest_pm25_time_col} = 7
+                WHERE date_column = '2023-07-20'
+            """
+
+    
+get_pm25_query_col_task = PythonOperator(
     task_id = 'check_latest_pm25',
     python_callable = query_latest_pm25,
     dag = dag
 )
 
-find_time_difference = PythonOperator(
-    task_id = 'find_time_difference',
-    python_callable=get_latest_pm25_result,
+get_pm25_query_value_task = PythonOperator(
+    task_id = 'check_latest_pm25_value',
+    python_callable = get_value_for_latest_pm25,
+    dag = dag
+)
+
+get_latest_pm25_api_task = PythonOperator(
+    task_id = 'get_latest_pm25_api',
+    python_callable=get_latest_pm25_api,
     dag=dag
 )
 
-get_pm25_query_results_task >> find_time_difference
+find_time_difference_task = PythonOperator(
+    task_id = 'find_pm25_time_difference',
+    python_callable=find_time_difference,
+    dag=dag
+)
+
+get_pm25_query_col_task >> get_pm25_query_value_task >> get_latest_pm25_api_task >> find_time_difference_task
